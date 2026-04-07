6ecf80968bd090076f2ec51bd1063ee209b446f4
nachito02 <arguellojuan08@gmail.com>
2026-04-07 04:05:58 -0300

    button texrt

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -65,10 +65,7 @@ export default function Hero() {
           transition={{ type: "spring", stiffness: 300 }}
           className="relative px-6 py-3 mt-4 font-semibold text-armelix-text bg-armelix-secondary  rounded-xl shadow-md overflow-hidden group"
         >
-          <span className="relative z-10">Quiero vender online</span>
-          <span className="absolute inset-0 text-white transition-all duration-300 opacity-0 group-hover:opacity-100 flex items-center justify-center">
-            Hablemos
-          </span>
+          <span className="relative z-10">Contactanos</span>
         </motion.a>
       </div>
 
